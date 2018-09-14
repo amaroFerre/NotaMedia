96ed4cfaa61bfef1e2f208b1a0cdb4adcebdc76a
Amarico <Amarico@equipo-zalman>
2018-09-14 18:23:34 +0200

    // Indexo el texto y hago el último push

--- src/notamedia/NotaMedia.java
+++ src/notamedia/NotaMedia.java
@@ -6,7 +6,6 @@
 package notamedia;
 
 // Importo el paquete java.util para poder utilizar la clase Scanner
-
 import java.util.*;
 
 /**
@@ -20,48 +19,38 @@ public class NotaMedia {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+
     //Realiza un programa que calcule la media de tres notas
-    
     // Creo un objeto de la clase Scanner
-    
     Scanner entrada = new Scanner(System.in);
-    
+
     // Creo cuatro variables de tipo double
-    
     double nota1, nota2, nota3;
-    
+
     double notaMedia;
-    
+
     // Pido las notas por teclado
-    
     System.out.println("Introduce la primera nota: ");
-    
+
     nota1 = entrada.nextDouble();
-    
+
     System.out.println("Introduce la segunda nota: ");
-    
+
     nota2 = entrada.nextDouble();
-    
+
     System.out.println("Introduce la tercera nota: ");
-    
+
     nota3 = entrada.nextDouble();
-    
+
     // Calculo la nota media
-    
     notaMedia = (nota1 + nota2 + nota3) / 3;
-    
+
     // Imprimo el resultado
-    System.out.print("La nota media es: " );
-    
+    System.out.print("La nota media es: ");
+
     // Imprime solo los dos primeros número decimales
     System.out.printf("%.2f\n", notaMedia);
-    
-    
-    
-    
-    
-    
+
   }
-  
+
 }
